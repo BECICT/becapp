cc8ad0490736c59f7e9dc9d733321dbcc50c6049
Okafor Ifenna <chibuike.okafor@netopng.com>
2021-12-15 16:57:16 +0100

    second push

--- src/extra/extra.module.ts
+++ src/extra/extra.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExtraService } from './extra.service';
 import { ExtraController } from './extra.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Extra } from './entities/extra.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Extra])],
   controllers: [ExtraController],
   providers: [ExtraService]
 })
